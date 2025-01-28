2f5b36807a62eaefcaae5728f133ce5d84be6e3e
Yoshi-tsuu <107516962+Yoshi-tsuu@users.noreply.github.com>
2025-01-28 18:54:08 +0100

    Update ApiGateway.java

--- APIGateway/ApiGateway.java
+++ APIGateway/ApiGateway.java
@@ -7,7 +7,6 @@ import java.util.Properties;
 
 public class ApiGateway {
     public static void main(String[] args) {
-
         Properties properties = new Properties();
         try {
             properties.load(new FileInputStream("config.properties"));
@@ -15,9 +14,7 @@ public class ApiGateway {
             System.err.println("Plik konfiguracyjny nie załadował się.");
             return;
         }
-
         int gatewayPort = Integer.parseInt(properties.getProperty("api.gateway.port"));
-
         try (ServerSocket serverSocket = new ServerSocket(gatewayPort)) {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
@@ -27,15 +24,12 @@ public class ApiGateway {
             System.err.println("Błąd połączenia z ApiGateway.");
         }
     }
-
     private static void processRequest(Socket clientSocket, Properties properties) {
         try (BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
              PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true)) {
-
             String request = input.readLine();
             String[] requestParts = request.split(";", 2);
             String requestType = requestParts[0];
-
             String targetServicePort;
             String targetServiceIP;
             switch (requestType) {
@@ -60,9 +54,7 @@ public class ApiGateway {
                     return;
                 }
             }
-
             int targetPort = Integer.parseInt(targetServicePort);
-
             if (requestType.equals("wgraj_plik")) {
                 try (Socket targetSocket = new Socket(targetServiceIP, targetPort);
                      PrintWriter targetOutput = new PrintWriter(targetSocket.getOutputStream(), true);
@@ -70,9 +62,7 @@ public class ApiGateway {
                     String[] parts = request.split(";");
                     String destinationFileName = parts[parts.length - 2];
                     String encodedFile = parts[parts.length - 1];
-
                     targetOutput.println("wgraj_plik;" + destinationFileName + ";" + encodedFile);
-
                     String response = targetInput.readLine();
                     output.print(response);
                     output.flush();
@@ -83,9 +73,7 @@ public class ApiGateway {
                 try (Socket targetSocket = new Socket(targetServiceIP, targetPort)) {
                     PrintWriter targetOutput = new PrintWriter(targetSocket.getOutputStream(), true);
                     BufferedReader targetInput = new BufferedReader(new InputStreamReader(targetSocket.getInputStream()));
-
                     targetOutput.println(request);
-
                     String response = targetInput.readLine();
                     output.println(response);
                     output.flush();
@@ -96,7 +84,6 @@ public class ApiGateway {
                 try (Socket targetSocket = new Socket(targetServiceIP, targetPort)) {
                     PrintWriter targetOutput = new PrintWriter(targetSocket.getOutputStream(), true);
                     BufferedReader targetInput = new BufferedReader(new InputStreamReader(targetSocket.getInputStream()));
-
                     targetOutput.println(request);
                     System.out.println("Otrzymano połączenie");
                     String response = targetInput.readLine();
